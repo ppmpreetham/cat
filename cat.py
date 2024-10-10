30f34979e75581aab4fc35e2eafc32cec00e67c0
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2024-10-10 14:56:28 +0530

    debugging

--- cat.py
+++ cat.py
@@ -40,13 +40,15 @@ def skip_space(s, idx):
             break
     return idx
 
+
 def parse_atom(s):
     import json
     try:
         return ['val', json.loads(s)]
     except json.JSONDecodeError:
         return s
-    
+
+
 def pl_parse(s):
     idx, node = parse_expr(s, 0)
     idx = skip_space(s, idx)
@@ -54,68 +56,71 @@ def pl_parse(s):
         raise ValueError('trailing garbage')
     return node
 
-def name_loopup(env, key):
+
+def name_lookup(env, key):
     while env:
+        if len(env) != 2:
+            raise ValueError('Invalid environment structure')
         current, env = env
         if key in current:
-            return current
-    raise ValueError('undefined name')
+            return current[key]
+    raise ValueError(f'undefined name: {key}')
 
-def pl_eval(env,node):
+
+def pl_eval(env, node):
     if not isinstance(node, list):
         assert isinstance(node, str)
-        return name_loopup(env, node)[node]
-    
+        return name_lookup(env, node)
+
     if len(node) == 0:
         raise ValueError('empty list')
 
-    # bool, number, string and etc
+    # bool, number, string, and etc.
     if len(node) == 2 and node[0] == 'val':
         return node[1]
-    
+
     # binary operators
     import operator
 
     binops = {
-    '+': operator.add,
-    '-': operator.sub,
-    '*': operator.mul,
-    '/': operator.truediv,
-    '=': operator.eq,
-    '!=': operator.ne,
-    '>=': operator.ge,
-    '>': operator.gt,
-    '<=': operator.le,
-    '<': operator.lt,
-    'and': operator.and_,
-    'or': operator.or_,
-    'xor': operator.xor,
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+        '/': operator.truediv,
+        '=': operator.eq,
+        '!=': operator.ne,
+        '>=': operator.ge,
+        '>': operator.gt,
+        '<=': operator.le,
+        '<': operator.lt,
+        'and': operator.and_,
+        'or': operator.or_,
+        'xor': operator.xor,
     }
 
     if len(node) == 3 and node[0] in binops:
         op = binops[node[0]]
-        return op(pl_eval(node[1]), pl_eval(node[2]))
-    
+        return op(pl_eval(env, node[1]), pl_eval(env, node[2]))
+
     # unary operators
     unops = {
-    '-': operator.neg,
-    'not': operator.not_,
-    '~': operator.not_,
+        '-': operator.neg,
+        'not': operator.not_,
+        '~': operator.not_,
     }
 
     if len(node) == 2 and node[0] in unops:
         op = unops[node[0]]
-        return op(pl_eval(node[1]))
+        return op(pl_eval(env, node[1]))
 
     if len(node) == 4 and node[0] == '?':
         _, cond, yes, no = node
-        if pl_eval(cond):
-            return pl_eval(yes)
+        if pl_eval(env, cond):
+            return pl_eval(env, yes)
         else:
-            return pl_eval(no)
-    
-    #If-Else
-    #do, then, else
+            return pl_eval(env, no)
+
+    # If-Else, do, then, else
     if node[0] in ('pounce', 'watch', 'purr') and len(node) > 1:
         new_env = (dict(), env)
         for val in node[1:]:
@@ -124,14 +129,13 @@ def pl_eval(env,node):
 
     # Print statement
     if node[0] == 'meow':
-        return print(*(pl_eval(val) for val in node[1:]))
+        return print(*(pl_eval(env, val) for val in node[1:]))
+
     raise ValueError('unknown expression')
 
 
 def evaluate(s):
-    return pl_eval(pl_parse(s))
+    return pl_eval(({},), pl_parse(s))
 
-string = str(input())
+string = input("Enter your expression (Lisp-like format, e.g., '(meow 1)'): ")
 print(evaluate(string))
-
-    
